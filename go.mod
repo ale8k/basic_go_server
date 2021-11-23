1cff68c0bc6060045f4aba20048548bf3aff882f
ale8k <alexanderkilroy@outlook.com>
2021-11-23 22:48:53 +0000

    Playing with mux
    Seems good, seems to have a bit more granularity and control, over things like spring boot
    and express

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ale8k/basic_go_server
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("bob")
-	for {
-	}
+
+	router := mux.NewRouter()
+
+	router.
+		Path("/testing").
+		Methods("GET").
+		HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			rw.Write([]byte("test"))
+		})
+
+	router.HandleFunc("/hi/{pathvar:[a-z]}/dude", func(rw http.ResponseWriter, r *http.Request) {
+		pathVarMap := mux.Vars(r)
+
+		fmt.Println("yo", pathVarMap["pathvar"], r.URL.Query().Get("yolo"))
+		rw.Write([]byte("hi"))
+	})
+
+	http.Handle("/", router)
+
+	fmt.Println("Server starting on port 8000...")
+	// Does it default to hostname of container?
+	fmt.Errorf(
+		"Server failed to start: ",
+		http.ListenAndServe(":8000", nil),
+	)
 }
