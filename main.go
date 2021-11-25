a92fb0949f58858b5e8d5a18e3b7e6d5ff744543
ale8k <alexanderkilroy@outlook.com>
2021-11-25 19:07:53 +0000

    remove comments

--- main.go
+++ main.go
@@ -24,24 +24,10 @@ func main() {
 	handlers.Consumer1 = Consumer1
 	routes.RegisterMessageQueue(router)
 
-	// router.
-	// 	Path("/testing").
-	// 	Methods("GET").
-	// 	HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-	// 		rw.Write([]byte("test"))
-	// 	})
-
-	// router.HandleFunc("/hi/{pathvar:[a-z]}/dude", func(rw http.ResponseWriter, r *http.Request) {
-	// 	pathVarMap := mux.Vars(r)
-
-	// 	fmt.Println("yo", pathVarMap["pathvar"], r.URL.Query().Get("yolo"))
-	// 	rw.Write([]byte("hi"))
-	// })
-
 	http.Handle("/", router)
 
 	fmt.Println("Server starting on port 8000...")
-	// Does it default to hostname of container?
+
 	log.Fatalf(
 		"Server failed to start: %v",
 		http.ListenAndServe(":8000", nil),
